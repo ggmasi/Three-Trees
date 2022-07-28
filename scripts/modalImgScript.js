c2aa7301c67288fa74c80fdae9267a6201d61030
Gustavo De Masi <97487206+ggmasi@users.noreply.github.com>
2022-07-28 14:34:57 -0300

    Add files via upload

---
+++ scripts/modalImgScript.js
@@ -0,0 +1,55 @@
+/*GOV SCRIPT-------------------------------------------------*/
+
+
+
+
+var govImg = document.getElementById("govImg");
+var gov = document.getElementById("gov")
+
+
+govImg.onmouseover = function(){
+  govImg.style.display = "none";
+  gov.style.display = "inline-block";
+}
+
+gov.onmouseleave = function(){
+  govImg.style.display = "block";
+  gov.style.display = "none";
+}
+
+/*GOV SCRIPT-------------------------------------------------*/
+
+/*IND SCRIPT-------------------------------------------------------*/ 
+var indImg = document.getElementById("indImg");
+var ind = document.getElementById("ind")
+
+
+indImg.onmouseover = function(){
+  indImg.style.display = "none";
+  ind.style.display = "inline-block";
+}
+
+ind.onmouseleave = function(){
+  indImg.style.display = "block";
+  ind.style.display = "none";
+}
+/*IND SCRIPT-----------------------------------------------------*/
+
+/*EMP SCRIPT--------------------------------------------------------------------*/
+
+var empImg = document.getElementById("empImg");
+var emp = document.getElementById("emp")
+
+
+empImg.onmouseover = function(){
+  empImg.style.display = "none";
+  emp.style.display = "inline-block";
+}
+
+emp.onmouseleave = function(){
+  empImg.style.display = "block";
+  emp.style.display = "none";
+}
+/*EMP SCRIPT--------------------------------------------------------------------*/
+
+
